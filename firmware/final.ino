7a7aa90b88305c79abcf4c6bf496ac1231b47f13
Miguel Moral <miguelmoralllamas@correo.ugr.es>
2017-09-11 20:00:21 +0200

    Realizando cambios menores

--- firmware/final.ino
+++ firmware/final.ino
@@ -5,13 +5,13 @@
 #include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
 
-//WIFI credentials go here
+//Credenciales WiFi
 const char* ssid     = "$SSID";
 const char* password = "$SSIDPass";
-//Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+
 // Iniciar ADS address con 0x48
-Adafruit_ADS1115 ads(0x48); //Con Adafruit_ADS1115 especificamos que es la versión de 16 bit.
-LiquidCrystal_I2C lcd(0x27, 16, 2); //Especificamos que la dirección del LCD es 0x27
+Adafruit_ADS1115 ads(0x48); //Con Adafruit_ADS1115 especificamos que es la versiï¿½n de 16 bit.
+LiquidCrystal_I2C lcd(0x27, 16, 2); //Especificamos que la direcciï¿½n del LCD es 0x27
 
 WiFiClient client;
 
@@ -35,37 +35,37 @@ double squareRoot(double fg){
 
 
 double calcIrms(unsigned int Number_of_Samples){
-  /* Be sure to update this value based on the IC and the gain settings! */
+
   float multiplier = 0.125F;    /* ADS1115 @ +/- 4.096V gain (16-bit results) */
-  //float multiplier = 0.1875F;
+
   Serial.println("*****************Inicio***************************");
   for (unsigned int n = 0; n < Number_of_Samples; n++)
   {
     sampleI = ads.readADC_Differential_0_1();
-    
-    // Elimina los 1,6V que tiene el sistema haciendo que la muestra esté centrada en 0 
+
+    // Elimina los 1,6V que tiene el sistema haciendo que la muestra estï¿½ centrada en 0
     offsetI = (offsetI + (sampleI-offsetI)/65536);
     filteredI = sampleI - offsetI;
-     
+
     // Root-mean-square method current
     // 1) square current values
     sqI = filteredI * filteredI;
 
-    // 2) sum 
+    // 2) sum
     sumI += sqI;
   }
 
     double I_RATIO = ICAL * multiplier;
-    Irms = squareRoot(sumI / Number_of_Samples)*I_RATIO; 
+    Irms = squareRoot(sumI / Number_of_Samples)*I_RATIO;
 
-    sumI = 0;      
+    sumI = 0;
 
-  
+
   return Irms;
 }
 
 void mandarJson(double irms, double potencia){
-  
+
    if (WiFi.status() == WL_CONNECTED) { //Comprobar wifi
 
     StaticJsonBuffer<300> JSONbuffer;   //Declarar JSON buffer
@@ -74,12 +74,12 @@ void mandarJson(double irms, double potencia){
     JSONencoder["sensorType"] = "Power";
     Serial.println("IRMS dentro de mandarjson: ");Serial.println(irms);
     JsonArray& values = JSONencoder.createNestedArray("values"); //JSON array
-    values.add(irms); //Añadir valor al array
-    values.add(potencia); //Añadir valor al array
+    values.add(irms); //Aï¿½adir valor al array
+    values.add(potencia); //Aï¿½adir valor al array
 
     JsonArray& valuesInfo = JSONencoder.createNestedArray("valuesInfo"); //JSON array
-    valuesInfo.add("irmsnuevo"); //Añadir valor al array
-    valuesInfo.add("potencianueva"); //Añadir valor al array
+    valuesInfo.add("irmsnuevo"); //Aï¿½adir valor al array
+    valuesInfo.add("potencianueva"); //Aï¿½adir valor al array
 
     char JSONmessageBuffer[300];
     JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
@@ -88,19 +88,19 @@ void mandarJson(double irms, double potencia){
     HTTPClient http;    //Declare object of class HTTPClient
 
     http.begin("$IPServidor");      //Especificar el destino de la solicitud
-    http.addHeader("Content-Type", "application/json");  
+    http.addHeader("Content-Type", "application/json");
 
     int httpCode = http.POST(JSONmessageBuffer);   //Mandar la solicitud
-    String payload = http.getString();           
+    String payload = http.getString();
 
-    Serial.println(httpCode);   //Imprime el código http
+    Serial.println(httpCode);   //Imprime el cï¿½digo http
     Serial.println(payload);    //Imprime el payload obtenido
 
-    http.end();  //Cierra la conexión
+    http.end();  //Cierra la conexiï¿½n
 
   } else {
 
-    Serial.println("Error en la conexión WIFI");
+    Serial.println("Error en la conexiï¿½n WIFI");
 
   }
 }
@@ -116,12 +116,12 @@ void imprimirLcd(double irms, double potencia){
 void setup() {
   Serial.begin(115200);
   delay(10);
-  
+
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  
-  // Conexión wifi
+
+  // Conexiï¿½n wifi
 
   Serial.println();
   Serial.print("Connecting to ");
@@ -129,7 +129,7 @@ void setup() {
   lcd.setCursor(0,1);
   lcd.print(ssid);
   Serial.println(ssid);
-  
+
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) { // No sale del bucle hasta que no conecte al WIFI
@@ -138,7 +138,7 @@ void setup() {
   }
 
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   lcd.print(WiFi.localIP());
@@ -148,19 +148,19 @@ void setup() {
 }
 
 void loop() {
-  
-   
+
+
  double irms = calcIrms(1000);
  double irmsaux = irms;
  double potencia = irms*220.0; //P=I*v  (Irms es la corriente eficaz)
- 
+
  Serial.println("IRMS: ");Serial.println(irms);
  Serial.println("Potencia: ");Serial.println(potencia);
 
- 
+
  mandarJson(irmsaux, potencia);
  imprimirLcd(irmsaux,potencia);
-    
+
 
 
 }
